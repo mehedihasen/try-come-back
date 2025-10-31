7b3744d610a2128630eae98a566ab6daba34887a
mehedi hasen <mehedisabuj4@gmail.com>
2025-10-31 23:34:42 +0600

    try to come back

---
+++ introduction.js
@@ -0,0 +1,22 @@
+let firstNumber = 22;
+let secondNumber = 22;
+
+let sum = (firstNumber + secondNumber);
+
+console.log(sum)
+
+
+
+let fruts =["banna","Mango","Jackfruts","Dragon"];
+
+
+function checkLanth(fruts){
+     let fruts = fruts
+     for(const frut of fruts){
+     console.log(frut)
+}
+     
+}
+
+let fall = checkLanth()
+console.log(fall)
